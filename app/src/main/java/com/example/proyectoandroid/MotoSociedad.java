631358c64d0ec9536179d2b9b4cbf89496428041
Denis <jesuskcinco@gmail.com>
2019-11-06 20:42:56 -0500

    Se crea el recicler para la bandeja

--- app/src/main/java/com/example/proyectoandroid/MotoSociedad.java
+++ app/src/main/java/com/example/proyectoandroid/MotoSociedad.java
@@ -2,13 +2,21 @@ package com.example.proyectoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
-public class MotoSociedad extends AppCompatActivity {
+import com.example.proyectoandroid.Utilidades.GlobalVariables;
 
+public class MotoSociedad extends AppCompatActivity {
+    SQLiteDatabase db;
+    ConexionSQLiteHelper con;
+    GlobalVariables globalVariables;
+    String dni;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moto_sociedad);
+        globalVariables = (GlobalVariables)getApplicationContext();
+        dni=globalVariables.getDni();
     }
 }
