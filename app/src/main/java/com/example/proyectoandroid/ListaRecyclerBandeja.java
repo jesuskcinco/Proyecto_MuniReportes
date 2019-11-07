5411cdf1f6a28a34ebc2f0382848dd01ff1baefb
Denis <jesuskcinco@gmail.com>
2019-11-06 20:46:29 -0500

    Se crea el recicler para la bandeja

---
+++ app/src/main/java/com/example/proyectoandroid/ListaRecyclerBandeja.java
@@ -0,0 +1,14 @@
+package com.example.proyectoandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaRecyclerBandeja extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_recycler_bandeja);
+    }
+}
